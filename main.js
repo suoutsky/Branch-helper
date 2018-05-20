2ce94df660a06d3ef707087671a87a4cd00fb48e
苏晓清 <>
2018-05-20 14:21:06 +0800

    branch

---
+++ main.js
@@ -0,0 +1,6 @@
+
+window.onload = function() {
+  var tag = document.getElementsByClassName('setting-main')[0];
+  tag.firstChild.style.height = '400px';
+  tag.firstChild.getElementsByTagName('select')[0].style.height = '400px';
+}
